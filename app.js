b769843953b106c24b8bca15150d44429f1b0007
Elijah Pereira <80046516+vahpour@users.noreply.github.com>
2021-11-08 09:54:54 -0500

    working update
    seperated functions
    added checkLetter function

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 var qwertyHTML = document.querySelector('#qwerty');
 var startBtn = document.querySelector('.btn__reset');
+var button = document.getElementsByTagName('button');
 var phrase = document.querySelector('#phrase');
 var missed = 0;
+var phraseArray = '';
 
 //3. hides the start screen overlay
 startBtn.addEventListener('click', () =>{
     const start = document.querySelector('.start');
     start.style.display = "none";
-    getRandomPhraseAsArray(phrases)
+    const inPhraseArray = getRandomPhraseAsArray(phrases);
+    phraseArray = inPhraseArray;
+    addPhraseToDisplay(inPhraseArray);
 });
 
 //4. creates an arary with 5 different strings.
@@ -19,24 +23,50 @@ var phrases = [
     "goodbye"
 ];
 //5. creates getRandomPhraseAsArray function.
-    function getRandomPhraseAsArray(arr){
-        //choose a phrase 
-        //split that phrase into a new array of characters
-        const innerPhrase = arr[Math.floor(Math.random()*arr.length)];
-        const splitPhrase = innerPhrase.split('');
-        
-        //do stuff to any arr that is passed in 
-        for (let i = 0; i<= splitPhrase.length; i++){
-            let phraseUL = document.querySelector('.phraseHolder');
-            let makeLI = document.createElement('li');
-            let splitChar = splitPhrase[i];
+function getRandomPhraseAsArray(arr){
+    const innerPhrase = arr[Math.floor(Math.random()*arr.length)];
+    const splitPhrase = innerPhrase.split('');
+    return splitPhrase;
+}
 
-            if(splitChar == ""){
-                makeLI.className = "space"
-            } else { 
-                makeLI.textContent = splitChar;
-                makeLI.className = "letter"
-            }
-            phraseUL.append(makeLI);
+//6. create an addPhraseToDisplay function.
+function addPhraseToDisplay(arr){
+    for (let i = 0; i<= arr.length; i++){
+        let phraseUL = document.querySelector('#phrase ul');
+        let makeLI = document.createElement('li');
+        let splitChar = arr[i];
+    
+        makeLI.textContent = splitChar;
+        if(splitChar == ' '){
+            makeLI.className = "space";
+        } else { 
+            makeLI.className = "letter";
         }
-    }
+        phraseUL.append(makeLI);
+}}
+
+//7. create a checkLetter function.
+function checkLetter(buttonClicked){
+    //get all elements with class "letter"
+    //loop over letters and check for matches
+    for (i=0; i< phraseArray.length; i++){
+        let targetLetter = phraseArray[i];
+        //if there's a match
+        //add "show" class to li
+        //store matching letter inside of a variable
+        //return the letter
+    if(buttonClicked.textContent === 'targetLetter'){
+        targetLetter.className = "show";
+        return targetLetter;
+    } else{return null;}
+}}
+
+//8. add an event listener to the keyboard.
+// button.addEventListener('click',(e) => {
+//     e.currentTarget.style.visibility = 'hidden';
+//   console.log(e.currentTarget);
+//     //when letter is picked add the “chosen” class to that button
+//     //set “disabled” attribute to true
+//     //pass the button to the checkLetter function
+//     //store the letter returned inside of a variable called letterFound
+// });
