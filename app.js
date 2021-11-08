7f238d805aa7eef75619eed974cd87613af32d67
Elijah Pereira <80046516+vahpour@users.noreply.github.com>
2021-11-08 08:17:58 -0500

    visible phrase
    implemented phrase styles
    phrase now displays when "startBtn" is clicked

--- app.js
+++ app.js
@@ -1,29 +1,42 @@
-document.addEventListener('DOMContentLoaded', (e) =>{
-    const qwerty = document.getElementById('qwerty');
-    const startBtn = document.querySelector('.btn__reset');
-    let missed = 0;
+var qwertyHTML = document.querySelector('#qwerty');
+var startBtn = document.querySelector('.btn__reset');
+var phrase = document.querySelector('#phrase');
+var missed = 0;
 
-    //3. hides the start screen overlay
-    startBtn.addEventListener('click', (e) =>{
+//3. hides the start screen overlay
+startBtn.addEventListener('click', () =>{
     const start = document.querySelector('.start');
     start.style.display = "none";
-    });
-    //4. creates an arary with 5 different strings.
-    var ski = [
-        'hello',
-        'thank you for coming',
-        'enjoy your stay',
-        'it was nice seeing you',
-        'goodbye'
-    ];
-    //5. creates getRandomPhraseAsArray function.
+    getRandomPhraseAsArray(phrases)
+});
+
+//4. creates an arary with 5 different strings.
+var phrases = [
+    "hello",
+    "thank you for coming",
+    "enjoy your stay",
+    "it was nice seeing you",
+    "goodbye"
+];
+//5. creates getRandomPhraseAsArray function.
     function getRandomPhraseAsArray(arr){
         //choose a phrase 
-        const arrRandomizer = arr[Math.floor(Math.random() * arr.length)];
-        const innerArr = arr[arrRandomizer];
         //split that phrase into a new array of characters
-        const splitArr = innerArr.split(',');
+        const innerPhrase = arr[Math.floor(Math.random()*arr.length)];
+        const splitPhrase = innerPhrase.split('');
+        
         //do stuff to any arr that is passed in 
-        return splitArr;
-    };
-});
+        for (let i = 0; i<= splitPhrase.length; i++){
+            let phraseUL = document.querySelector('.phraseHolder');
+            let makeLI = document.createElement('li');
+            let splitChar = splitPhrase[i];
+
+            if(splitChar == ""){
+                makeLI.className = "space"
+            } else { 
+                makeLI.textContent = splitChar;
+                makeLI.className = "letter"
+            }
+            phraseUL.append(makeLI);
+        }
+    }
