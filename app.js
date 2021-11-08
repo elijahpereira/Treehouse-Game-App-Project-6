336445ef2f2eca70ef310c60e3fc3a14b3ce00da
Elijah Pereira <80046516+vahpour@users.noreply.github.com>
2021-11-08 10:41:02 -0500

    updated scope
    moved start to global scope

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var qwertyHTML = document.querySelector('#qwerty');
 var startBtn = document.querySelector('.btn__reset');
+var start = document.querySelector('.start');
 var button = document.getElementsByTagName('button');
 var phrase = document.querySelector('#phrase');
 var missed = 0;
@@ -7,7 +8,6 @@ var phraseArray = '';
 
 //3. hides the start screen overlay
 startBtn.addEventListener('click', () =>{
-    const start = document.querySelector('.start');
     start.style.display = "none";
     const inPhraseArray = getRandomPhraseAsArray(phrases);
     phraseArray = inPhraseArray;
@@ -16,11 +16,11 @@ startBtn.addEventListener('click', () =>{
 
 //4. creates an arary with 5 different strings.
 var phrases = [
-    "hello",
-    "thank you for coming",
-    "enjoy your stay",
-    "it was nice seeing you",
-    "goodbye"
+    "thank you next",
+    "goodnight n go",
+    "bloodline",
+    "just like magic",
+    "into you"
 ];
 //5. creates getRandomPhraseAsArray function.
 function getRandomPhraseAsArray(arr){
@@ -45,30 +45,36 @@ function addPhraseToDisplay(arr){
         phraseUL.append(makeLI);
 }}
 
-//7. create a checkLetter function.
-function checkLetter(buttonClicked){
-    //get all elements with class "letter"
-    //loop over letters and check for matches
-    for (let i=0; i< phraseArray.length; i++){
-        let targetLetter = phraseArray[i];
-        //if there's a match
-        //add "show" class to li
-        //store matching letter inside of a variable
-        //return the letter
-    if(buttonClicked.textContent === 'targetLetter'){
-        targetLetter.className = "show";
-        return targetLetter;
-    } else{return null;}
-}}
+// //7. create a checkLetter function.
+// function checkLetter(letter){
+//     phraseArray
+//     //get all elements with class "letter"
+//     //loop over letters and check for matches
+//     for (let i=0; i< phraseArray.length; i++){
+//         let targetLetter = phraseArray[i];
+//         //if there's a match
+//         //add "show" class to li
+//         //store matching letter inside of a variable
+//         //return the letter
+//     if(letter.textContent = targetLetter){
+//         targetLetter.className = "show";
+//         return targetLetter;
+//     } else{return null;}
+// }}
 
-//8. add an event listener to the keyboard.
-
-// for ( )
-// button.addEventListener('click',(e) => {
-//     e.currentTarget.style.visibility = 'hidden';
-//   console.log(e.currentTarget);
+// //8. add an event listener to the keyboard.
+// qwerty.addEventListener('click',(e) => {
 //     //when letter is picked add the “chosen” class to that button
 //     //set “disabled” attribute to true
+//     if (e.target.tagName != 'BUTTON' || e.target.className == 'chosen'){
+//         console.log('hi');
+//     }
+//     if (e.target.tagName === 'BUTTON'){
+//         e.target.className = 'chosen';
+//         e.target.disabled = true;
+//         checkLetter(e.target);
+//         let letterFound = checkLetter(e.target);
+//     }
 //     //pass the button to the checkLetter function
 //     //store the letter returned inside of a variable called letterFound
 // });
