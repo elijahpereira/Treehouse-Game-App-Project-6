4e727110ec3797ed215a893ba874d50bfc2d1274
Elijah Pereira <80046516+vahpour@users.noreply.github.com>
2021-11-09 17:12:46 -0500

    implemented lost heart clause

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
-var qwertyHTML = document.querySelector('#qwerty');
+const qwertyHTML = document.querySelector('#qwerty');
 const startBtn = document.querySelector('.btn__reset');
-let phraseUL = document.querySelector('#phrase ul');
+const phraseUL = document.querySelector('#phrase ul');
+const checkablePhrase = document.getElementsByClassName("letter");
 const button = document.getElementsByTagName('button');
+const scoreboardUL = document.querySelector('#scoreboard ol');
+
+const firstHeart = document.getElementsByClassName('.tries');
+const createIMG = document.createElement('img');
+const createLI = document.createElement('li');
 
 let missed = 0;
 let phraseArray = '';
@@ -46,33 +52,52 @@ function addPhraseToDisplay(arr){
 }}
 
 //7. create a checkLetter function.
-function checkLetter(letter){
-    //get all elements with class "letter"
-    let check = document.getElementsByClassName('letter');
+function checkLetter(button){
+    let match = '';
     //loop over letters and check for matches
-    for (let i=0; i< phraseArray.length; i++){
-        let targetLetter = phraseArray[i];
-        //if there's a match
-        //add "show" class to li
-        //store matching letter inside of a variable
-        //return the letter
-    if(letter.textContent = targetLetter){
-        targetLetter.className = "show";
-        return targetLetter;
+    //if there's a match
+    //add "show" class to li
+    //store matching letter inside of a variable
+    //return the letter
+    for (let i=0; i< checkablePhrase.length; i++){
+        let targetLetter = checkablePhrase[i].textContent;
+        if(button.textContent === targetLetter){
+            checkablePhrase[i].className += " show";
+            match = button.textContent;
+        }
     }
-    console.log(check);
-}}
+    if (match.length> 0){
+        return match;
+    } else {
+        return null;
+    }
+}
 
 //8. add an event listener to the keyboard.
 qwerty.addEventListener('click',(e) => {
+    let letterFound = checkLetter(e.target);
     //when letter is picked add the “chosen” class to that button
     //set “disabled” attribute to true
+    //pass the button to the checkLetter function
+    //store the letter returned inside of a variable called letterFound
     if (e.target.tagName === 'BUTTON'){
         e.target.className = 'chosen';
         e.target.disabled = true;
-        checkLetter(e.target);
-        let letterFound = checkLetter(e.target);
+        letterFound;
+        //9. count the missed guesses in the game.
+        //write a statement to check the value of the letterFound variable.
+        //if the value is null, remove one of the tries from the scoreboard
+        //when you remove a try from the scoreboard ,increase the missed count by 1
+        //change a liveHeart.png image to a lostHeart.png image
+        if (letterFound === null){
+            scoreboardUL.removeChild(scoreboardUL.children[missed]);
+            createLI.classList = "tries";
+            createIMG.src = "images/lostHeart.png";
+            createIMG.style.height = '35px';
+            createIMG.style.width = '30px';
+            createLI.appendChild(createIMG); 
+            scoreboardUL.insertBefore(createLI, scoreboardUL.children[missed]);
+            missed++; 
+        }
     }
-    //pass the button to the checkLetter function
-    //store the letter returned inside of a variable called letterFound
 });
